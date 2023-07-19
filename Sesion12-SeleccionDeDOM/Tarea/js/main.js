c4d3ea2406f6803593af9a3ea3ea3d5aef2545c9
MoisesGJ <mgarciaj1802@alumno.ipn.mx>
2023-07-18 22:05:30 -0600

    Add sess

--- Sesion12-SeleccionDeDOM/Tarea/js/main.js
+++ Sesion12-SeleccionDeDOM/Tarea/js/main.js
@@ -61,7 +61,7 @@ const koders = [
   },
 ];
 
-const createListItem = (koderObject) => {
+/* const createListItem = (koderObject) => {
   const { name, lastname } = koderObject;
   const fullName = `${name} ${lastname}`;
   const listItem = document.createElement('li');
@@ -77,7 +77,7 @@ const printKodersList = (kodersList) => {
   });
 };
 printKodersList(koders);
-
+ */
 /*
     ya que este impresa la lista, hacer que los li pares tengan un color de fondo, y los nones otro color diferente
 */
@@ -93,15 +93,13 @@ const changeColorToEvenOrOdd = () => {
   });
 };
 
-changeColorToEvenOrOdd();
-
 /*
     Crear una nueva lista que contenga el nombre completo del koder y su promedio
     si el promedio del koder es menor que 9.5, ese texto (el del promedio) debe ser color amarillo
     si es mayor o igual, debe ser verde
 */
 
-const averageKoders = (arraykoders) =>
+/* const averageKoders = (arraykoders) =>
   arraykoders.reduce((accum, { name, lastname, scores }) => {
     const { html, css, js } = scores;
     const average = parseFloat((html + css + js) / 3).toFixed(2);
@@ -134,3 +132,100 @@ const printKodersListAverage = (arraykoders) => {
 };
 
 printKodersListAverage(averageKoders(koders));
+ */
+
+// Todo: Simple
+
+const createListSpanItem = (arrayproperties, listtag) => {
+  arrayproperties.forEach((property) => {
+    const spantag = document.createElement('span');
+    spantag.innerHTML = ` ${property}`;
+    listtag.appendChild(spantag);
+  });
+
+  return listtag;
+};
+
+const createListItemKoder = (arraykoder) => {
+  const litag = document.createElement('li');
+  litag.innerText = `${arraykoder[0]} ${arraykoder[1]}`;
+
+  if (arraykoder.length > 2) {
+    const extra = arraykoder.splice(2, arraykoder.length);
+    createListSpanItem(extra, litag);
+  }
+
+  return litag;
+};
+
+const printListKodersAll = (arraykoders, properties, idtag) => {
+  const list = document.getElementById(idtag);
+  arraykoders.forEach((koder) => {
+    const error = false;
+    const scores = koder.scores;
+    let send = [koder.name, koder.lastname];
+
+    if (properties.length > 2) {
+      const aux = { ...koder };
+      aux.delete.name;
+      const extras = properties.map((property) => {
+        console.log(property);
+        scores[property];
+      });
+
+      send = send.concat(extras);
+    }
+
+    console.log(send);
+    const koderElement = createListItemKoder(send);
+    list.appendChild(koderElement);
+  });
+};
+
+printListKodersAll(koders, ['name', 'lastname', 'scores'], 'koders-list');
+
+/*const printKodersList = (kodersList) => {
+  const list = document.getElementById('koders-list');
+  kodersList.forEach((koder) => {
+    const koderElement = createListItem(koder);
+    list.appendChild(koderElement);
+  });
+};
+printKodersList(koders);
+
+ const averageKoders = (arraykoders) =>
+  arraykoders.reduce((accum, { name, lastname, scores }) => {
+    const { html, css, js } = scores;
+    const average = parseFloat((html + css + js) / 3).toFixed(2);
+
+    return [...accum, { fullname: `${name} ${lastname}`, average }];
+  }, []);
+
+const createKodersListAverage = (koderObject) => {
+  const { fullname, average } = koderObject;
+  const listItem = document.createElement('li');
+  listItem.innerText = fullname + ' ';
+  const spanaverage = document.createElement('span');
+  spanaverage.innerText = average;
+
+  average < 9.5
+    ? spanaverage.classList.add('yellow')
+    : spanaverage.classList.add('green');
+
+  listItem.appendChild(spanaverage);
+
+  return listItem;
+};
+
+const printKodersListAverage = (arraykoders) => {
+  const list = document.getElementById('koders-list-average');
+  arraykoders.forEach((koder) => {
+    const koderElement = createKodersListAverage(koder);
+    list.appendChild(koderElement);
+  });
+};
+
+printKodersListAverage(averageKoders(koders));
+
+changeColorToEvenOrOdd();
+ */
